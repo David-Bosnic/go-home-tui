8d39372572464e410aff471affcc3a98f4e630c0
David <davidbosnic99@gmail.com>
2025-09-18 12:49:10 -0600

    split up main.go into more files

---
+++ setup.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Setup() {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		os.Exit(1)
+	}
+	url := configDir + "/go-home/.env"
+	err = godotenv.Load(url)
+	if err != nil {
+		createConfig()
+		fmt.Println("Creating Config in user config {USER_CONFIG}/go-home/.env")
+		fmt.Println("Use README.md to config your credentials")
+		os.Exit(1)
+	}
+	authFlag := flag.Bool("a", false, "Open Google Oauth on the Browser")
+	flag.Parse()
+	if *authFlag {
+		fmt.Println("> Opening Google Oauth using default browser\n> http://localhost:8080/auth/google")
+		OpenUrl("http://localhost:8080/auth/google")
+		OauthSpinUp()
+	} else {
+		err := RefreshOauth(apiConf)
+		if err != nil {
+			log.Printf("Failed to refresh Oauth %e", err)
+			os.Exit(1)
+		}
+	}
+}
